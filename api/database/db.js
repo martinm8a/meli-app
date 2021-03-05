19a7e65f38dd040b7952f9229112357d13f2d1ec
Martin Ochoa <martinm8a@gmail.com>
2021-03-05 13:58:16 -0300

    back

--- api/database/db.js
+++ api/database/db.js
@@ -1,24 +1,24 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const {
-  DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env;
-
-
-
-const {Sequelize} = require ("sequelize")
+// const {
+//   DB_USER, DB_PASSWORD, DB_HOST,
+// } = process.env;
 
 
-const sequelize = new Sequelize(process.env.DATABASE_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/meli`, {
+
+// const {Sequelize} = require ("sequelize")
+
+
+// const sequelize = new Sequelize(process.env.DATABASE_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/meli`, {
   
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-    ssl: {
-        require: true,
-        rejectUnauthorized: false
-    }
-  }
-});
+//   dialect: 'postgres',
+//   protocol: 'postgres',
+//   dialectOptions: {
+//     ssl: {
+//         require: true,
+//         rejectUnauthorized: false
+//     }
+//   }
+// });
 
-module.exports = sequelize
+// module.exports = sequelize
--- api/index.js
+++ api/index.js
@@ -1,47 +1,13 @@
-var express =  require ('express');
-const sequelize = require ('./database/db.js')
-const bodyParser = require("body-parser")
-const server = express()
-const axios = require("axios");
+const cors = require("cors")
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(cors());
+app.use(express.json())
+app.use(bodyParser.json());
+app.use(require("./routes/ruta.js"))
 
 
-
-// Ruta hacia el endpoint >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-server.get("/api/search", (req, res) => {
-  const xProduct = req.query.q;
-  axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${xProduct}`)
-    .then((xProducts) => {
-      const arrayProd = xProducts.data.results;
-      let products = arrayProd.map((product) => {
-        return {
-          id: product.id,
-          title: product.title,
-          price: product.price,
-          currency_id: product.currency_id,
-          available_quantity: product.available_quantity,
-          thumbnail: product.thumbnail,
-          condition: product.condition,
-        };
-      });
-      res.send(products);
-    })
-    .catch((err) => {
-      return res.send(err, console.log("Hay un error")).status(400);
-    });
-});
-
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-server.listen(3001, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
-    sequelize
-              .authenticate()
-              .then(() => {
-                console.log('Se ha conectado correctamente.');
-              })
-              .catch(err => {
-                console.error('No se puede conectar a la base de datos:', err);
-              });
-  });
-  
-  console.log('Server esuchando en puerto: 3001');
+app.listen(5001,
+console.log(">>Console: server listenning at port 5001 "))
